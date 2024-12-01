f976ab8e4f20926c86b7a7e470fc48271d146f88
piyush <piyush72717272@gmail.com>
2024-12-01 20:03:37 +0530

    fixed errors

--- bouncy_bargains_app/lib/utils/constants/enums.dart
+++ bouncy_bargains_app/lib/utils/constants/enums.dart
@@ -6,7 +6,7 @@
 
 
 /// Switch of Custom Brand-Text-Size Widget
-enum TextSizes { small, medium, large }
+enum TexXSizes { small, medium, large }
 
 enum OrderStatus { processing, shipped, delivered }
 
