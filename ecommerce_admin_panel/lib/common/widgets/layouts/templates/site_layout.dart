d41d572a7cdd7e547a892c153ae9f39557a01b7b
piyush <piyush72717272@gmail.com>
2024-12-05 22:26:01 +0530

    site layout

---
+++ ecommerce_admin_panel/lib/common/widgets/layouts/templates/site_layout.dart
@@ -0,0 +1,51 @@
+import 'package:ecommerce_admin_panel/common/widgets/layouts/headers/header.dart';
+import 'package:ecommerce_admin_panel/common/widgets/responsive/responsive_design.dart';
+import 'package:ecommerce_admin_panel/common/widgets/responsive/screens/desktop_layout.dart';
+import 'package:ecommerce_admin_panel/common/widgets/responsive/screens/mobile_layout.dart';
+import 'package:ecommerce_admin_panel/common/widgets/responsive/screens/tablet_layout.dart';
+import 'package:flutter/material.dart';
+
+// Template for the overall site layout , responsive to different screen sizes
+class TSiteTemplate extends StatelessWidget {
+  const TSiteTemplate(
+      {super.key,
+      this.desktop,
+      this.tablet,
+      this.mobile,
+      this.useLayout = false});
+
+  // Widget for desktop layout
+  final Widget? desktop;
+
+  // Widget for tablet layout
+  final Widget? tablet;
+
+  // Widget for mobile layout
+  final Widget? mobile;
+
+  // Flag to determine whether to use the layout
+  final bool useLayout;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: THeader(),
+        body: TResponsiveWidget(
+          desktop: useLayout
+              ? DesktopLayout(
+                  body: desktop,
+                )
+              : desktop ?? Container(),
+          tablet: useLayout
+              ? TabletLayout(
+                  body: tablet ?? desktop,
+                )
+              : tablet ?? desktop ?? Container(),
+          mobile: useLayout
+              ? MobileLayout(
+                  body: mobile ?? desktop,
+                )
+              : mobile ?? desktop ?? Container(),
+        ));
+  }
+}
