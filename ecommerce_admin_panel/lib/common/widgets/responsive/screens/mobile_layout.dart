1c69dff20bd2faf6b4042c0e4090cf699637d604
piyush <piyush72717272@gmail.com>
2024-12-05 22:25:38 +0530

    mobile layout

---
+++ ecommerce_admin_panel/lib/common/widgets/responsive/screens/mobile_layout.dart
@@ -0,0 +1,22 @@
+import 'package:ecommerce_admin_panel/common/widgets/layouts/headers/header.dart';
+import 'package:ecommerce_admin_panel/common/widgets/layouts/sidebars/sidebar.dart';
+import 'package:flutter/material.dart';
+
+class MobileLayout extends StatelessWidget {
+  MobileLayout({super.key, this.body});
+
+  final Widget? body;
+  final GlobalKey<ScaffoldState> scaffoldKey = GlobalKey();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      key: scaffoldKey,
+      drawer: TSidebar(),
+      appBar: THeader(
+        scaffoldKey: scaffoldKey,
+      ),
+      body: body ?? SizedBox(),
+    );
+  }
+}
