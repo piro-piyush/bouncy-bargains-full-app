92e027e917d2ece1304b3349a9f94f1dcac8ea15
piyush <piyush72717272@gmail.com>
2024-12-05 22:25:02 +0530

    app routes

---
+++ ecommerce_admin_panel/lib/routes/routes.dart
@@ -0,0 +1,44 @@
+
+class TRoutes {
+
+  static const firstScreen = '/';
+  static const responsiveDesignTutorialScreen = '/responsive-tutorial-design-screen';
+  static const secondScreen = '/second-screen';
+  static const secondScreenWithUID = '/second-screen/:userId';
+
+  static List sideMenuItems = [
+    firstScreen,
+    responsiveDesignTutorialScreen
+  ];
+
+
+  static const login = '/login';
+  static const forgetPassword = '/forgetPassword';
+  static const resetPassword = '/resetPassword';
+  // static const dashboard = '/dashboard';
+  // static const media = '/media';
+  //
+  // static const banners = '/banners';
+  // static const createBanner = '/createBanner';
+  // static const editBanner = '/editBanner';
+  //
+  // static const products = '/products';
+  // static const createProducts = '/createProducts';
+  // static const editProducts = '/editProducts';
+  //
+  // static const brands = '/brands';
+  // static const createBrand = '/createBrand';
+  // static const editBrand = '/editBrand';
+  //
+  // static const categories = '/categories';
+  // static const createCategory = '/createCategory';
+  // static const editCategory = '/editCategory';
+  //
+  // static const customers = '/customers';
+  // static const createCustomer = '/createCustomer';
+  // static const customerDetails = '/customerDetails';
+  //
+  // static const orders = '/orders';
+  //
+  // static const responsiveDesign = '/responsiveDesign';
+}
