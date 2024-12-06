5b31b0d546f2cebd3a3e3f3afb657d101a70d71d
piyush <piyush72717272@gmail.com>
2024-12-06 22:41:25 +0530

    routes

--- ecommerce_admin_panel/lib/routes/routes.dart
+++ ecommerce_admin_panel/lib/routes/routes.dart
@@ -1,44 +1,35 @@
-
 class TRoutes {
-
-  static const firstScreen = '/';
-  static const responsiveDesignTutorialScreen = '/responsive-tutorial-design-screen';
-  static const secondScreen = '/second-screen';
-  static const secondScreenWithUID = '/second-screen/:userId';
-
   static List sideMenuItems = [
-    firstScreen,
-    responsiveDesignTutorialScreen
+    dashboard,
   ];
 
-
   static const login = '/login';
-  static const forgetPassword = '/forgetPassword';
-  static const resetPassword = '/resetPassword';
-  // static const dashboard = '/dashboard';
-  // static const media = '/media';
-  //
-  // static const banners = '/banners';
-  // static const createBanner = '/createBanner';
-  // static const editBanner = '/editBanner';
-  //
-  // static const products = '/products';
-  // static const createProducts = '/createProducts';
-  // static const editProducts = '/editProducts';
-  //
-  // static const brands = '/brands';
-  // static const createBrand = '/createBrand';
-  // static const editBrand = '/editBrand';
-  //
-  // static const categories = '/categories';
-  // static const createCategory = '/createCategory';
-  // static const editCategory = '/editCategory';
-  //
-  // static const customers = '/customers';
-  // static const createCustomer = '/createCustomer';
-  // static const customerDetails = '/customerDetails';
-  //
-  // static const orders = '/orders';
-  //
-  // static const responsiveDesign = '/responsiveDesign';
+  static const forgetPassword = '/forget-password/';
+  static const resetPassword = '/reset-password';
+  static const dashboard = '/dashboard';
+// static const media = '/media';
+//
+// static const banners = '/banners';
+// static const createBanner = '/createBanner';
+// static const editBanner = '/editBanner';
+//
+// static const products = '/products';
+// static const createProducts = '/createProducts';
+// static const editProducts = '/editProducts';
+//
+// static const brands = '/brands';
+// static const createBrand = '/createBrand';
+// static const editBrand = '/editBrand';
+//
+// static const categories = '/categories';
+// static const createCategory = '/createCategory';
+// static const editCategory = '/editCategory';
+//
+// static const customers = '/customers';
+// static const createCustomer = '/createCustomer';
+// static const customerDetails = '/customerDetails';
+//
+// static const orders = '/orders';
+//
+// static const responsiveDesign = '/responsiveDesign';
 }
--- ecommerce_admin_panel/lib/routes/routes_middleware.dart
+++ ecommerce_admin_panel/lib/routes/routes_middleware.dart
@@ -1,13 +1,13 @@
-
+import 'package:ecommerce_admin_panel/data/repositories/authentication/authentication_repository.dart';
 import 'package:ecommerce_admin_panel/routes/routes.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get_navigation/src/routes/route_middleware.dart';
 
 class TRoutesMiddleware extends GetMiddleware {
-
   @override
   RouteSettings? redirect(String? route) {
-    final isAuthenticated = true;
-    return isAuthenticated? null: const RouteSettings(name: TRoutes.firstScreen);
+    return AuthenticationRepository.instance.isAuthenticated
+        ? null
+        : RouteSettings(name: TRoutes.login);
   }
 }
