1cd22de16b7d9c33ca2d6bec429dc50a48a420a3
piyush <piyush72717272@gmail.com>
2024-12-06 22:40:02 +0530

    dashboard screen

---
+++ ecommerce_admin_panel/lib/features/shop/screens/dashboard/dashboard_screen.dart
@@ -0,0 +1,20 @@
+import 'package:ecommerce_admin_panel/common/widgets/layouts/templates/site_layout.dart';
+import 'package:ecommerce_admin_panel/features/shop/screens/dashboard/responsive_screen/dashboard_desktop_screen.dart';
+import 'package:ecommerce_admin_panel/features/shop/screens/dashboard/responsive_screen/dashboard_mobile_screen.dart';
+import 'package:ecommerce_admin_panel/features/shop/screens/dashboard/responsive_screen/dashboard_tablet_screen.dart';
+
+import 'package:flutter/material.dart';
+
+class DashboardScreen extends StatelessWidget {
+  const DashboardScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return TSiteTemplate(
+      useLayout: false,
+      desktop: DashboardDesktopScreen(),
+      tablet: DashboardTabletScreen(),
+      mobile: DashboardMobileScreen(),
+    );
+  }
+}
