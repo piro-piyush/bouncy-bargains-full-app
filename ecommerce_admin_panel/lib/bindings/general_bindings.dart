933bc66830f1d9884fde87ae62fe938eca3d0579
piyush <piyush72717272@gmail.com>
2024-12-07 22:29:33 +0530

    general bindings

---
+++ ecommerce_admin_panel/lib/bindings/general_bindings.dart
@@ -0,0 +1,12 @@
+import 'package:ecommerce_admin_panel/features/authentication/controllers/user_controller.dart';
+import 'package:ecommerce_admin_panel/utils/helpers/network_manager.dart';
+import 'package:get/get.dart';
+
+class GeneralBindings extends Bindings {
+  @override
+  void dependencies() {
+    // Core
+    Get.lazyPut(() => NetworkManager(), fenix: true);
+    Get.lazyPut(() => UserController(), fenix: true);
+  }
+}
