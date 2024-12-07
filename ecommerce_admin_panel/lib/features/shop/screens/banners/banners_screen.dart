2bad798d50bdf87691495a6fdb1868feebb053e3
piyush <piyush72717272@gmail.com>
2024-12-07 22:28:20 +0530

    banners screen

---
+++ ecommerce_admin_panel/lib/features/shop/screens/banners/banners_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class BannersScreen extends StatelessWidget {
+  const BannersScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
