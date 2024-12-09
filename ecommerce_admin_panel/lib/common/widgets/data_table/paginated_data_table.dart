fe406edcd35764d6513836044b19aeeab5d18d10
piyush <piyush72717272@gmail.com>
2024-12-09 22:08:39 +0530

    paginated_data_table.dart

--- ecommerce_admin_panel/lib/common/widgets/data_table/paginated_data_table.dart
+++ ecommerce_admin_panel/lib/common/widgets/data_table/paginated_data_table.dart
@@ -56,7 +56,8 @@ class TPaginatedDataTable extends StatelessWidget {
       height: tableHeight,
       child: Theme(
         // Use to set the Backend color
-        data: Theme.of(context).copyWith(cardTheme: const CardTheme(color: Colors.white, elevation: 0)),
+        data: Theme.of(context).copyWith(
+            cardTheme: const CardTheme(color: Colors.white, elevation: 0)),
         child: PaginatedDataTable2(
           source: source,
           columns: columns,
@@ -74,8 +75,13 @@ class TPaginatedDataTable extends StatelessWidget {
           onRowsPerPageChanged: (noOfRows) {},
           sortColumnIndex: sortColumnIndex,
           headingTextStyle: Theme.of(context).textTheme.titleMedium,
-          headingRowColor: WidgetStateProperty.resolveWith((states) => TColors.primaryBackground),
-          empty: TAnimationLoaderWidget(animation: TImages.packageAnimation, text: 'Nothing Found', height: 200, width: 200),
+          headingRowColor: WidgetStateProperty.resolveWith(
+              (states) => TColors.primaryBackground),
+          empty: TAnimationLoaderWidget(
+              animation: TImages.packageAnimation,
+              text: 'Nothing Found',
+              height: 200,
+              width: 200),
           headingRowDecoration: const BoxDecoration(
             borderRadius: BorderRadius.only(
               topLeft: Radius.circular(TSizes.borderRadiusMd),
@@ -84,7 +90,8 @@ class TPaginatedDataTable extends StatelessWidget {
           ),
           sortArrowBuilder: (bool ascending, bool sorted) {
             if (sorted) {
-              return Icon(ascending ? Iconsax.arrow_up_3 : Iconsax.arrow_down, size: TSizes.iconSm);
+              return Icon(ascending ? Iconsax.arrow_up_3 : Iconsax.arrow_down,
+                  size: TSizes.iconSm);
             } else {
               return const Icon(Iconsax.arrow_3, size: TSizes.iconSm);
             }
