1892b239c03e2022d29c98bc59f811a95a7fcd8b
piyush <piyush72717272@gmail.com>
2024-12-09 22:08:10 +0530

    dashboard mobile screen

--- ecommerce_admin_panel/lib/features/shop/screens/dashboard/responsive_screen/dashboard_mobile_screen.dart
+++ ecommerce_admin_panel/lib/features/shop/screens/dashboard/responsive_screen/dashboard_mobile_screen.dart
@@ -1,3 +1,5 @@
+import 'package:ecommerce_admin_panel/features/shop/screens/dashboard/widgets/dashboard_card.dart';
+import 'package:ecommerce_admin_panel/utils/constants/sizes.dart';
 import 'package:flutter/material.dart';
 
 class DashboardMobileScreen extends StatelessWidget {
@@ -5,6 +7,56 @@ class DashboardMobileScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: EdgeInsets.all(TSizes.defaultSpace),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              // Heading
+              Text(
+                'Dashboard',
+                style: Theme.of(context).textTheme.headlineLarge,
+              ),
+              SizedBox(
+                height: TSizes.spaceBtwSections,
+              ),
+
+              // Cards
+              TDashboardCard(
+                stats: 25,
+                title: 'Sales Total',
+                subTitle: '\$333.3',
+              ),
+              SizedBox(
+                height: TSizes.spaceBtwItems,
+              ),
+              TDashboardCard(
+                stats: 15,
+                title: 'Average order Value',
+                subTitle: '\$24',
+              ),
+              SizedBox(
+                height: TSizes.spaceBtwItems,
+              ),
+              TDashboardCard(
+                stats: 44,
+                title: 'Total orders',
+                subTitle: '32',
+              ),
+              SizedBox(
+                height: TSizes.spaceBtwItems,
+              ),
+              TDashboardCard(
+                stats: 2,
+                title: 'Visitors',
+                subTitle: '23,322',
+              )
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
