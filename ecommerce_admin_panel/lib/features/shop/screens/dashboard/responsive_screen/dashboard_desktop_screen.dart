0c5553a49ccf214e6c05dbff538a828b050c58cd
piyush <piyush72717272@gmail.com>
2024-12-11 14:53:42 +0530

    dashboard desktop screen

--- ecommerce_admin_panel/lib/features/shop/screens/dashboard/responsive_screen/dashboard_desktop_screen.dart
+++ ecommerce_admin_panel/lib/features/shop/screens/dashboard/responsive_screen/dashboard_desktop_screen.dart
@@ -1,13 +1,19 @@
 import 'package:ecommerce_admin_panel/common/widgets/containers/rounded_container.dart';
+import 'package:ecommerce_admin_panel/features/shop/controllers/dashboard/dashboard_controller.dart';
 import 'package:ecommerce_admin_panel/features/shop/screens/dashboard/widgets/dashboard_card.dart';
+import 'package:ecommerce_admin_panel/utils/constants/colors.dart';
 import 'package:ecommerce_admin_panel/utils/constants/sizes.dart';
+import 'package:ecommerce_admin_panel/utils/device/device_utility.dart';
+import 'package:fl_chart/fl_chart.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 class DashboardDesktopScreen extends StatelessWidget {
   const DashboardDesktopScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final controller = Get.put(DashboardController());
     return Scaffold(
       body: SingleChildScrollView(
         child: Padding(
@@ -18,10 +24,7 @@ class DashboardDesktopScreen extends StatelessWidget {
               // Heading
               Text(
                 'Dashboard',
-                style: Theme
-                    .of(context)
-                    .textTheme
-                    .headlineLarge,
+                style: Theme.of(context).textTheme.headlineLarge,
               ),
               SizedBox(
                 height: TSizes.spaceBtwSections,
@@ -69,16 +72,78 @@ class DashboardDesktopScreen extends StatelessWidget {
                   ),
                 ],
               ),
-              const SizedBox(height: TSizes.spaceBtwSections,),
+              const SizedBox(
+                height: TSizes.spaceBtwSections,
+              ),
 
               // Graphs
               Row(
                 children: [
-                  Expanded(flex: 2,
+                  Expanded(
+                    flex: 2,
                     child: Column(
                       children: [
                         // Bar graph
-                        TRoundedContainer(),
+                        TRoundedContainer(
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                "Weekly Sales",
+                                style:
+                                    Theme.of(context).textTheme.headlineSmall,
+                              ),
+                              const SizedBox(
+                                height: TSizes.spaceBtwSections,
+                              ),
+
+                              // Graph
+                              SizedBox(
+                                  height: 400,
+                                  child: BarChart(BarChartData(
+                                      titlesData: buildFlTitlesData(),
+                                      borderData: FlBorderData(
+                                          show: true,
+                                          border: Border(
+                                            top: BorderSide.none,
+                                            right: BorderSide.none,
+                                          )),
+                                      gridData: FlGridData(
+                                        show: true,
+                                        drawHorizontalLine: true,
+                                        drawVerticalLine: true,
+                                        horizontalInterval: 200,
+                                      ),
+                                      barGroups: controller.weeklySales
+                                          .asMap()
+                                          .entries
+                                          .map((entry) => BarChartGroupData(
+                                                  x: entry.key,
+                                                  barRods: [
+                                                    BarChartRodData(
+                                                        width: 30,
+                                                        toY: entry.value,
+                                                        color: TColors.primary,
+                                                        borderRadius:
+                                                            BorderRadius
+                                                                .circular(
+                                                                    TSizes.sm))
+                                                  ]))
+                                          .toList(),
+                                      groupsSpace: TSizes.spaceBtwItems,
+                                      barTouchData: BarTouchData(
+                                          touchTooltipData: BarTouchTooltipData(
+                                              getTooltipColor: (_) =>
+                                                  TColors.secondary),
+                                          touchCallback:
+                                              TDeviceUtils.isDesktopScreen(
+                                                      context)
+                                                  ? (barTouchEvent,
+                                                      barTouchResponse) {}
+                                                  : null))))
+                            ],
+                          ),
+                        ),
                         SizedBox(
                           height: TSizes.spaceBtwSections,
                         ),
@@ -103,94 +168,33 @@ class DashboardDesktopScreen extends StatelessWidget {
   }
 }
 
-// class MyData extends DataTableSource {
-//   final DashboardController controller = Get.put(DashboardController());
-//
-//   @override
-//   DataRow? getRow(int index) {
-//     final data = controller.filteredDataList[index];
-//     return DataRow2(
-//         onTap: () {},
-//         selected: controller.selectedRows[index],
-//         onSelectChanged: (value) =>
-//             controller.selectedRows[index] = value ?? false,
-//         cells: [
-//           DataCell(Text(data['Column1'] ?? '')),
-//           DataCell(Text(data['Column2'] ?? '')),
-//           DataCell(Text(data['Column3'] ?? '')),
-//           DataCell(Text(data['Column4'] ?? '')),
-//         ]);
-//   }
-//
-//   @override
-//   bool get isRowCountApproximate => false;
-//
-//   @override
-//   int get rowCount => controller.filteredDataList.length;
-//
-//   @override
-//   int get selectedRowCount => 0;
-// }
-//
-// class DashboardController extends GetxController {
-//   var dataList = <Map<String, dynamic>>[].obs;
-//   var filteredDataList = <Map<String, dynamic>>[].obs;
-//   RxList<bool> selectedRows =
-//       <bool>[].obs; // Observable list to store selected rows
-//
-//   RxInt sortColumnIndex =
-//       1.obs; // Observable for tracking the index of the column for sorting
-//   RxBool sortAscending = true
-//       .obs; // Observable for tracking the sorting order (ascending or descending)
-//   final searchTextController = TextEditingController();
-//
-//   @override
-//   void onInit() {
-//     super.onInit();
-//     fetchDummyData();
-//   }
-//
-//   void searchQuery(String query) {
-//     filteredDataList.assignAll(dataList
-//         .where((item) => item['column1'].contains(query.toLowerCase())));
-//   }
-//
-//   void sortById(int sortColumnIndex, bool ascending) {
-//     sortAscending.value = ascending;
-//     filteredDataList.sort((a, b) {
-//       if (ascending) {
-//         return filteredDataList[0]['column1']
-//             .toString()
-//             .toLowerCase()
-//             .compareTo(filteredDataList[0]['column1'].toString().toLowerCase());
-//       } else {
-//         return filteredDataList[0]['column1']
-//             .toString()
-//             .toLowerCase()
-//             .compareTo(filteredDataList[0]['column1'].toString().toLowerCase());
-//       }
-//     });
-//     this.sortColumnIndex.value = sortColumnIndex;
-//   }
-//
-//   void fetchDummyData() {
-//     selectedRows.assignAll(
-//         List.generate(36, (index) => false)); // Initialize selected rows
-//     dataList.addAll(List.generate(
-//         36,
-//         (index) => {
-//               'Column1': 'Data ${index + 1} -1',
-//               'Column2': 'Data ${index + 1} -2',
-//               'Column3': 'Data ${index + 1} -3',
-//               'Column4': 'Data ${index + 1} -4',
-//             }));
-//     filteredDataList.addAll(List.generate(
-//         36,
-//         (index) => {
-//               'Column1': 'Data ${index + 1} -1',
-//               'Column2': 'Data ${index + 1} -2',
-//               'Column3': 'Data ${index + 1} -3',
-//               'Column4': 'Data ${index + 1} -4',
-//             }));
-//   }
-// }
+FlTitlesData buildFlTitlesData() {
+  return FlTitlesData(
+      show: true,
+      bottomTitles: AxisTitles(
+          sideTitles: SideTitles(
+              showTitles: true,
+              getTitlesWidget: (value, meta) {
+                // Map index to the desired day of the week
+                final days = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'];
+
+                // Calculate the index and ensure it wraps around for the correct day
+                final index = value.toInt() % days.length;
+
+                // Get the day corresponding to the calculated index
+                final day = days[index];
+                return SideTitleWidget(
+                  axisSide: AxisSide.bottom,
+                  space: 0,
+                  child: Text(day),
+                );
+              })),
+      leftTitles: AxisTitles(
+          sideTitles:
+              SideTitles(showTitles: true, interval: 200, reservedSize: 50)),
+      rightTitles: AxisTitles(
+          sideTitles: SideTitles(
+        showTitles: false,
+      )),
+      topTitles: AxisTitles(sideTitles: SideTitles(showTitles: false)));
+}
