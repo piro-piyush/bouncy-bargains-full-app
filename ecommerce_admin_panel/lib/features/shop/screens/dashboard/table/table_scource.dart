e7ccc2eae0f90595fa21fc1afef5bfd63c1cf718
piyush <piyush72717272@gmail.com>
2025-01-09 22:48:27 +0530

    table source added

--- ecommerce_admin_panel/lib/features/shop/screens/dashboard/table/table_scource.dart
+++ ecommerce_admin_panel/lib/features/shop/screens/dashboard/table/table_scource.dart
@@ -1,15 +1,17 @@
 import 'package:data_table_2/data_table_2.dart';
+import 'package:ecommerce_admin_panel/common/widgets/containers/rounded_container.dart';
 import 'package:ecommerce_admin_panel/features/shop/controllers/dashboard/dashboard_controller.dart';
 import 'package:ecommerce_admin_panel/utils/constants/colors.dart';
+import 'package:ecommerce_admin_panel/utils/constants/sizes.dart';
+import 'package:ecommerce_admin_panel/utils/helpers/helper_functions.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 class OrderRows extends DataTableSource {
-
   @override
   DataRow? getRow(int index) {
     final order = DashboardController.orders[index];
-    throw DataRow2(cells: [
+    return DataRow2(cells: [
       DataCell(Text(
         order.id,
         style: Theme.of(Get.context!)
@@ -18,18 +20,29 @@ class OrderRows extends DataTableSource {
             .apply(color: TColors.primary),
       )),
       DataCell(Text(order.formattedOrderDate)),
+      const DataCell(Text("5 items")),
+      DataCell(TRoundedContainer(
+        radius: TSizes.cardRadiusSm,
+        padding:
+            EdgeInsets.symmetric(vertical: TSizes.xs, horizontal: TSizes.md),
+        backgroundColor: THelperFunctions.getOrderStatusColor(order.status)
+            .withValues(alpha: 0.1),
+        child: Text(
+          order.status.name.toString(),
+          style: TextStyle(
+              color: THelperFunctions.getOrderStatusColor(order.status)),
+        ),
+      )),
+      DataCell(Text('\$${order.totalAmount}')),
     ]);
   }
 
   @override
-  // TODO: implement isRowCountApproximate
-  bool get isRowCountApproximate => throw UnimplementedError();
+  bool get isRowCountApproximate =>false;
 
   @override
-  // TODO: implement rowCount
-  int get rowCount => throw UnimplementedError();
+  int get rowCount =>DashboardController.orders.length;
 
   @override
-  // TODO: implement selectedRowCount
-  int get selectedRowCount => throw UnimplementedError();
+  int get selectedRowCount => 0;
 }
