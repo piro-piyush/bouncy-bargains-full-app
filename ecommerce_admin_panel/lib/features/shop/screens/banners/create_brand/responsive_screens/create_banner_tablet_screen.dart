cf45e873a54528c70ac185164d104b88a123bd7e
piyush <piyush72717272@gmail.com>
2025-01-10 22:41:08 +0530

    create banner features

---
+++ ecommerce_admin_panel/lib/features/shop/screens/banners/create_brand/responsive_screens/create_banner_tablet_screen.dart
@@ -0,0 +1,36 @@
+import 'package:ecommerce_admin_panel/common/widgets/breadcrumbs/breadcrumb_with_heading.dart';
+import 'package:ecommerce_admin_panel/features/shop/screens/banners/create_brand/widgets/create_banner_form.dart';
+import 'package:ecommerce_admin_panel/routes/routes.dart';
+import 'package:ecommerce_admin_panel/utils/constants/sizes.dart';
+import 'package:flutter/material.dart';
+
+class CreateBannerTabletScreen extends StatelessWidget {
+  const CreateBannerTabletScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(TSizes.defaultSpace),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              // Breadcrumbs
+              TBreadcrumbWithHeading(
+                  returnToPreviousScreen: true,
+                  heading: "Create Banner",
+                  breadcrumbItems: [TRoutes.brands, "Create Banner"]),
+              SizedBox(
+                height: TSizes.spaceBtwSections,
+              ),
+
+              // Form
+              CreateBannerForm(),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
