84d62a7f98b6cd51c27743e4205853ff357af99f
piyush <piyush72717272@gmail.com>
2025-01-10 22:38:48 +0530

    brands features

---
+++ ecommerce_admin_panel/lib/features/shop/screens/brands/all_brands/responsive_screens/brands_tablet_screen.dart
@@ -0,0 +1,20 @@
+import 'package:ecommerce_admin_panel/utils/constants/sizes.dart';
+import 'package:flutter/material.dart';
+
+class BrandsTabletScreen extends StatelessWidget {
+  const BrandsTabletScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(TSizes.defaultSpace),
+          child: Column(
+            children: [],
+          ),
+        ),
+      ),
+    );
+  }
+}
