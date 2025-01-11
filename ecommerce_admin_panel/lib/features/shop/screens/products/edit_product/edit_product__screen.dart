e1509787b50fc9ce1d8e1b19fe3153276aaa39ca
piyush <piyush72717272@gmail.com>
2025-01-11 22:32:17 +0530

    fixed everything till customer screen

--- ecommerce_admin_panel/lib/features/shop/screens/products/edit_product/edit_product__screen.dart
+++ ecommerce_admin_panel/lib/features/shop/screens/products/edit_product/edit_product__screen.dart
@@ -2,7 +2,6 @@ import 'package:ecommerce_admin_panel/common/widgets/layouts/templates/site_layo
 import 'package:ecommerce_admin_panel/features/shop/screens/products/edit_product/responsive_screens/edit_product_desktop_screen.dart';
 import 'package:ecommerce_admin_panel/features/shop/screens/products/edit_product/responsive_screens/edit_product_mobile_screen.dart';
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
 import 'responsive_screens/edit_product_tablet_screen.dart';
 
 class EditProductScreen extends StatelessWidget {
