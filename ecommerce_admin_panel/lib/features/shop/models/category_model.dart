4ddeb1fbfb2eabe76925205c12395dcdd74910aa
piyush <piyush72717272@gmail.com>
2025-01-14 21:52:12 +0530

    all fixes

--- ecommerce_admin_panel/lib/features/shop/models/category_model.dart
+++ ecommerce_admin_panel/lib/features/shop/models/category_model.dart
@@ -1,30 +1,65 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:ecommerce_admin_panel/utils/formatters/formatter.dart';
+
 class CategoryModel {
   String id;
   String name;
   String image;
+  String parentId;
+  bool isFeatured;
+  DateTime? createdAt;
+  DateTime? updatedAt;
 
   // Constructor
   CategoryModel({
     required this.id,
     required this.name,
     required this.image,
+    this.isFeatured = false,
+    this.parentId = "",
+    this.createdAt,
+    this.updatedAt,
   });
 
-  // Factory method to create a CategoryModel from JSON
-  factory CategoryModel.fromJson(Map<String, dynamic> json) {
-    return CategoryModel(
-      id: json['id'] ?? '',
-      name: json['name'] ?? '',
-      image: json['image'] ?? '',
-    );
+  String get formattedDate => TFormatter.formatDate(createdAt);
+
+  String get formattedUpdatedAtDate => TFormatter.formatDate(updatedAt);
+
+  // Empty Helper Function
+  static CategoryModel empty() =>
+      CategoryModel(id: "", name: "", image: "", isFeatured: false);
+
+  // Factory method to create a CategoryModel from Firestore DocumentSnapshot
+  factory CategoryModel.fromSnapShot(
+      DocumentSnapshot<Map<String, dynamic>> document) {
+    if (document.data() != null) {
+      final data = document.data()!;
+      return CategoryModel(
+        id: document.id,
+        name: data['Name'] ?? '',
+        image: data['Image'] ?? '',
+        parentId: data['ParentId'] ?? '',
+        isFeatured: data['IsFeatured'] ?? false,
+        createdAt:
+            data.containsKey("CreatedAt") ? data["CreatedAt"]?.toDate() : null,
+        updatedAt:
+            data.containsKey("UpdatedAt") ? data["UpdatedAt"]?.toDate() : null,
+      );
+    } else {
+      return CategoryModel.empty();
+    }
   }
 
   // Method to convert CategoryModel to JSON
-  Map<String, dynamic> toJson() {
+  toJson() {
     return {
-      'id': id,
-      'name': name,
-      'image': image,
+      'Name': name,
+      'Image': image,
+      'ParentId': parentId,
+      'IsFeatured': isFeatured,
+      'CreatedAt': createdAt,
+      'UpdatedAt': updatedAt = DateTime.now(),
+      // Assigning DateTime.now() only if updatedAt is null
     };
   }
 }
