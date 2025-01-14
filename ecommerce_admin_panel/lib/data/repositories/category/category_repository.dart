a3a3e11cfc821c7ca55472a19a8c48f48b61ace6
piyush <piyush72717272@gmail.com>
2025-01-14 21:52:14 +0530

    all fixes

---
+++ ecommerce_admin_panel/lib/data/repositories/category/category_repository.dart
@@ -0,0 +1,26 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:ecommerce_admin_panel/features/shop/models/category_model.dart';
+import 'package:ecommerce_admin_panel/utils/exceptions/firebase_exceptions.dart';
+import 'package:get/get.dart';
+
+class CategoryRepository extends GetxController {
+  static CategoryRepository get instance => Get.find();
+
+  final FirebaseFirestore _db = FirebaseFirestore.instance;
+
+  // Get all categories from the "categories" collection
+  Future<List<CategoryModel>> getAllCategories() async {
+    try {
+      final snapshot = await _db.collection("Categories").get();
+      final result =
+          snapshot.docs.map((doc) => CategoryModel.fromSnapShot(doc)).toList();
+      return result;
+    } on FirebaseException catch (e) {
+      throw TFirebaseException(e.code).message;
+    } on FirebaseException catch (e) {
+      throw TFirebaseException(e.code).message;
+    } catch (e) {
+      throw 'Something went wrong. Please try again';
+    }
+  }
+}
