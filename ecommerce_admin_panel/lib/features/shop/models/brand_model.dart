ff16be7c99fd3b071466f1d40fc13ca9a95fb766
piyush <piyush72717272@gmail.com>
2025-01-15 12:16:08 +0530

    brand model fixes

--- ecommerce_admin_panel/lib/features/shop/models/brand_model.dart
+++ ecommerce_admin_panel/lib/features/shop/models/brand_model.dart
@@ -1,30 +1,55 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
 class BrandModel {
   String id;
   String name;
   String image;
+  bool isFeatured;
+  int productsCount;
 
   // Constructor
   BrandModel({
     required this.id,
     required this.name,
     required this.image,
+    this.isFeatured = false,
+    this.productsCount = 0,
   });
 
-  // Factory method to create a BrandModel from JSON
-  factory BrandModel.fromJson(Map<String, dynamic> json) {
-    return BrandModel(
-      id: json['id'] ?? '',
-      name: json['name'] ?? '',
-      image: json['image'] ?? '',
-    );
+  // Factory method to create a BrandModel from Firestore DocumentSnapshot
+  factory BrandModel.fromSnapshot(DocumentSnapshot<Map<String, dynamic>> document) {
+    if (document.data() != null) {
+      final data = document.data()!;
+      return BrandModel(
+        id: document.id,
+        name: data['Name'] ?? '',
+        image: data['Image'] ?? '',
+        isFeatured: data['IsFeatured'] ?? false,
+        productsCount: data.containsKey('ProductsCount') ? data['ProductsCount'] ?? 0 : 0,
+      );
+    } else {
+      return BrandModel.empty();
+    }
   }
 
   // Method to convert BrandModel to JSON
   Map<String, dynamic> toJson() {
     return {
-      'id': id,
-      'name': name,
-      'image': image,
+      'Name': name,
+      'Image': image,
+      'IsFeatured': isFeatured,
+      'ProductsCount': productsCount,
     };
+  }
+
+  // Empty Brand Helper Function
+  static BrandModel empty() {
+    return BrandModel(
+      id: '',
+      name: '',
+      image: '',
+      isFeatured: false,
+      productsCount: 0,
+    );
   }
 }
