d52458029cafe5feca4cd13a9e16f9515032b2ac
piyush <piyush72717272@gmail.com>
2025-01-15 12:16:27 +0530

    category fixes

--- ecommerce_admin_panel/lib/data/repositories/category/category_repository.dart
+++ ecommerce_admin_panel/lib/data/repositories/category/category_repository.dart
@@ -1,6 +1,8 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:ecommerce_admin_panel/features/shop/models/category_model.dart';
 import 'package:ecommerce_admin_panel/utils/exceptions/firebase_exceptions.dart';
+import 'package:ecommerce_admin_panel/utils/exceptions/platform_exceptions.dart';
+import 'package:flutter/services.dart';
 import 'package:get/get.dart';
 
 class CategoryRepository extends GetxController {
@@ -12,15 +14,14 @@ class CategoryRepository extends GetxController {
   Future<List<CategoryModel>> getAllCategories() async {
     try {
       final snapshot = await _db.collection("Categories").get();
-      final result =
-          snapshot.docs.map((doc) => CategoryModel.fromSnapShot(doc)).toList();
+      final result = snapshot.docs.map((doc) => CategoryModel.fromSnapShot(doc)).toList();
       return result;
     } on FirebaseException catch (e) {
       throw TFirebaseException(e.code).message;
-    } on FirebaseException catch (e) {
-      throw TFirebaseException(e.code).message;
+    } on PlatformException catch (e) {
+      throw TPlatformException(e.code).message;
     } catch (e) {
-      throw 'Something went wrong. Please try again';
+      throw 'Something went wrong. Please try again ${e.toString()}';
     }
   }
 }
