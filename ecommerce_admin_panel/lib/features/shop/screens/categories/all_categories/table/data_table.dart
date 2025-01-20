6d2732e990853bcb874da8b87dacf7ec2cd2d6bc
piyush <piyush72717272@gmail.com>
2025-01-20 20:57:29 +0530

    data table update

--- ecommerce_admin_panel/lib/features/shop/screens/categories/all_categories/table/data_table.dart
+++ ecommerce_admin_panel/lib/features/shop/screens/categories/all_categories/table/data_table.dart
@@ -10,20 +10,27 @@ class CategoryTable extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final controller = CategoryController.instance;
-    return Obx(
-      ()=> TPaginatedDataTable(
-        sortAscending: controller.sortAscending.value,
+    final controller = Get.put(CategoryController());
+    return Obx(() {
+      Text(controller.filteredItems.length.toString());
+      return TPaginatedDataTable(
+          sortAscending: controller.sortAscending.value,
           sortColumnIndex: controller.sortColumnIndex.value,
           minWidth: 700,
           columns: [
-            DataColumn2(label: Text("Category"),onSort:(columnIndex,ascending) => controller.sortByName(columnIndex,ascending)),
-            DataColumn2(label: Text("Parent Category"),onSort:(columnIndex,ascending) => controller.sortByName(columnIndex,ascending)),
+            DataColumn2(
+                label: Text("Category"),
+                onSort: (columnIndex, ascending) =>
+                    controller.sortByName(columnIndex, ascending)),
+            DataColumn2(
+                label: Text("Parent Category"),
+                onSort: (columnIndex, ascending) =>
+                    controller.sortByName(columnIndex, ascending)),
             DataColumn2(label: Text("Featured")),
             DataColumn2(label: Text("Date")),
             DataColumn2(label: Text("Action"), fixedWidth: 100),
           ],
-          source: CategoryRows()),
-    );
+          source: CategoryRows());
+    });
   }
 }
