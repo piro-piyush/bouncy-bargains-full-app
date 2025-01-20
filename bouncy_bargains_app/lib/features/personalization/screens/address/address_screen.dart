17e9783ea5919f33b281b34cffa126c47a2f8884
piyush <piyush72717272@gmail.com>
2025-01-20 20:56:13 +0530

    address feature

--- bouncy_bargains_app/lib/features/personalization/screens/address/address_screen.dart
+++ bouncy_bargains_app/lib/features/personalization/screens/address/address_screen.dart
@@ -1,8 +1,10 @@
 import 'package:bouncy_bargain/common/widgets/appbar/appbar.dart';
+import 'package:bouncy_bargain/features/personalization/controllers/address_controller.dart';
 import 'package:bouncy_bargain/features/personalization/screens/address/add_new_address_screen.dart';
 import 'package:bouncy_bargain/features/personalization/screens/address/widgets/single_address.dart';
 import 'package:bouncy_bargain/utils/constants/colors.dart';
 import 'package:bouncy_bargain/utils/constants/sizes.dart';
+import 'package:bouncy_bargain/utils/helpers/cloud_helper_functions.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:iconsax/iconsax.dart';
@@ -12,7 +14,44 @@ class AddressScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final controller = Get.put(AddressController());
     return Scaffold(
+      appBar: XAppBar(
+        title: Text(
+          "Addresses",
+          style: Theme.of(context).textTheme.headlineSmall,
+        ),
+        showBackArrow: true,
+      ),
+      body: SingleChildScrollView(
+        child: Padding(
+            padding: const EdgeInsets.all(XSizes.defaultSpace),
+            child: Column(
+              children: [
+                Obx(
+                  () => FutureBuilder(
+                      key: Key(controller.refreshData.value.toString()),
+                      future: controller.getAllUserAddress(),
+                      builder: (context, snapshot) {
+                        final response =
+                            XCloudHelperFunctions.checkMultiRecordState(
+                                snapshot: snapshot);
+                        if (response != null) return response;
+
+                        final addresses = snapshot.data!;
+                        return ListView.builder(
+                            shrinkWrap: true,
+                            itemCount: addresses.length,
+                            itemBuilder: (_, index) => XSingleAddress(
+                                  address: addresses[index],
+                                  onTap: () => controller
+                                      .selectAddress(addresses[index]),
+                                ));
+                      }),
+                ),
+              ],
+            )),
+      ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
           Get.to(() => const AddNewAddressScreen());
@@ -22,33 +61,6 @@ class AddressScreen extends StatelessWidget {
           Iconsax.add,
           color: XColors.white,
         ),
-      ),
-      appBar: XAppBar(
-        title: Text(
-          "Addresses",
-          style: Theme.of(context).textTheme.headlineSmall,
-        ),
-        showBackArrow: true,
-      ),
-      body: const SingleChildScrollView(
-        child: Padding(
-            padding: EdgeInsets.all(XSizes.defaultSpace),
-            child: Column(
-              children: [
-                XSingleAddress(
-                  selectedAddress: true,
-                ),
-                XSingleAddress(
-                  selectedAddress: false,
-                ),
-                XSingleAddress(
-                  selectedAddress: false,
-                ),
-                XSingleAddress(
-                  selectedAddress: false,
-                ),
-              ],
-            )),
       ),
     );
   }
