0684c692ec7a8045424269c1382eb7f5160aba3e
piyush <piyush72717272@gmail.com>
2025-01-20 20:58:12 +0530

    onboarding update

--- bouncy_bargains_app/lib/features/authentication/controllers/onboarding/onboarding_controllers.dart
+++ bouncy_bargains_app/lib/features/authentication/controllers/onboarding/onboarding_controllers.dart
@@ -6,39 +6,48 @@ import 'package:get_storage/get_storage.dart';
 class OnboardingController extends GetxController {
   static OnboardingController get instance => Get.find();
 
-  /// Variables
+  /// Page controller for the onboarding screens
   final pageController = PageController();
+
+  /// Storage instance to manage first-time user flag
   final storage = GetStorage();
+
+  /// Reactive variable to track the current page index
   Rx<int> currentPageIndex = 0.obs;
 
-  // Update current index when page scroll.
-  void updatePageIndicator(index) => currentPageIndex.value = index;
+  /// Update the current page index when the user scrolls between pages
+  void updatePageIndicator(int index) {
+    currentPageIndex.value = index;
+  }
 
-  // Jump to specific selected dot page.
-  void dotNavigationClick(index) {
+  /// Navigate to a specific page when a dot indicator is clicked
+  void dotNavigationClick(int index) {
     currentPageIndex.value = index;
     pageController.jumpToPage(index);
   }
 
-  //  Update current index and jump to next page
+  /// Go to the next page in the onboarding process
   void nextPage() {
     if (currentPageIndex.value == 2) {
-      // Making local storage variable to false so we can't see onboarding screen if we have skipped once
+      // When the last page is reached, set the flag to false to skip onboarding on future app launches
       storage.write('isFirstTime', false);
-      Get.offAll(const LoginScreen());
+      Get.offAll(
+          const LoginScreen()); // Redirect to Login screen after onboarding
     } else {
+      // Move to the next page
       int page = currentPageIndex.value + 1;
       pageController.jumpToPage(page);
     }
   }
 
-  //  Update current index and jump to last page
+  /// Skip the onboarding process and jump directly to the last page
   void skipPage() {
+    // Set the current page to the last page and update the index
     currentPageIndex.value = 2;
     pageController.jumpToPage(2);
 
-    // Making local storage variable to false so we can't see onboarding screen if we have skipped once
+    // Set the flag in storage so the onboarding doesn't show on future app launches
     storage.write('isFirstTime', false);
-    Get.to(const LoginScreen());
+    Get.to(const LoginScreen()); // Redirect to Login screen
   }
 }
