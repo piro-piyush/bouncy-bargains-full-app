7b1e32d3d2a85cbd57c87b12cbf9a2b597cf6ff8
piyush <piyush72717272@gmail.com>
2025-01-30 15:25:30 +0530

    category feature

---
+++ ecommerce_admin_panel/lib/features/shop/controllers/category/edit_category_controller.dart
@@ -0,0 +1,105 @@
+import 'package:ecommerce_admin_panel/data/repositories/category/category_repository.dart';
+import 'package:ecommerce_admin_panel/features/media/controllers/media_controller.dart';
+import 'package:ecommerce_admin_panel/features/media/models/image_model.dart';
+import 'package:ecommerce_admin_panel/features/shop/controllers/category/category_controller.dart';
+import 'package:ecommerce_admin_panel/features/shop/models/category_model.dart';
+import 'package:ecommerce_admin_panel/utils/helpers/network_manager.dart';
+import 'package:ecommerce_admin_panel/utils/popups/full_screen_loader.dart';
+import 'package:ecommerce_admin_panel/utils/popups/loaders.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:get/get.dart';
+
+class EditCategoryController extends GetxController {
+  static EditCategoryController get instance => Get.find();
+
+  final selectedParent = CategoryModel.empty().obs;
+  final isLoading = false.obs;
+  RxString imageUrl = "".obs;
+  final isFeatured = false.obs;
+  final name = TextEditingController();
+  final formKey = GlobalKey<FormState>();
+  final _controller = CategoryController.instance;
+
+  /// Init Data
+  void init(CategoryModel category) {
+    name.text = category.name;
+    isFeatured.value = category.isFeatured;
+    imageUrl.value = category.image;
+    if (category.parentId.isNotEmpty) {
+      selectedParent.value = _controller.allItems
+              .firstWhereOrNull((e) => e.id == category.parentId) ??
+          CategoryModel.empty();
+    }
+  }
+
+  // Method to reset fields;
+  void resetFields() {
+    selectedParent(CategoryModel.empty());
+    isLoading(false);
+    isFeatured(false);
+    name.clear();
+    imageUrl.value = "";
+  }
+
+  // Pick Thumbnail Image from Media
+  void pickImage() async {
+    final controller = Get.put(MediaController());
+    List<ImageModel>? selectedImages = await controller.selectImagesFromMedia();
+
+    // Handle the selected Images
+    if (selectedImages != null && selectedImages.isNotEmpty) {
+      // Set the selected images to the main page or perform any other action
+      ImageModel selectedImage = selectedImages.first;
+      // Update the main image using the selectedImage
+      imageUrl.value = selectedImage.url;
+    }
+  }
+
+  // Register new Category
+  Future<void> updateCategory(CategoryModel category) async {
+    try {
+      // Start Loading
+      TFullScreenLoader.popUpCircular();
+
+      // Check Internet connectivity
+      final isConnected = await NetworkManager.instance.isConnected();
+      if (!isConnected) {
+        TFullScreenLoader.stopLoading();
+        return;
+      }
+      if (!formKey.currentState!.validate()) {
+        TFullScreenLoader.stopLoading();
+        return;
+      }
+
+      // Map Data
+      category.image = imageUrl.value;
+      category.name = name.text.trim();
+      category.parentId = selectedParent.value.id;
+      category.isFeatured = isFeatured.value;
+      category.updatedAt = DateTime.now();
+
+      // Call the repo to update category
+      await CategoryRepository.instance.updateCategory(categoryModel: category);
+
+      // Update All Data List
+      _controller.updateItemInList(category);
+
+      // Reset Form
+      resetFields();
+
+      // Remove Loader
+      TFullScreenLoader.stopLoading();
+
+      // Back
+      Get.back();
+
+      // Success message & Redirect
+      TLoaders.successSnackBar(
+          title: "Congratulations", message: "New record has been added");
+    } catch (e) {
+      TFullScreenLoader.stopLoading();
+      TLoaders.errorSnackBar(title: "Oh snap!", message: e.toString);
+    }
+  }
+}
