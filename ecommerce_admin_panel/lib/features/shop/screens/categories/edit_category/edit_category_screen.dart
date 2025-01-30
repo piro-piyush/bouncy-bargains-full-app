728b2084ffccbf9773f0ac6a9524f9064cdc9f3f
piyush <piyush72717272@gmail.com>
2025-01-30 15:25:52 +0530

    category feature

--- ecommerce_admin_panel/lib/features/shop/screens/categories/edit_category/edit_category_screen.dart
+++ ecommerce_admin_panel/lib/features/shop/screens/categories/edit_category/edit_category_screen.dart
@@ -1,26 +1,32 @@
+import 'dart:developer';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:ecommerce_admin_panel/common/widgets/layouts/templates/site_layout.dart';
 import 'package:ecommerce_admin_panel/features/shop/screens/categories/edit_category/responsive_screens/edit_categories_desktop_screen.dart';
 import 'package:ecommerce_admin_panel/features/shop/screens/categories/edit_category/responsive_screens/edit_categories_mobile_screen.dart';
 import 'package:ecommerce_admin_panel/features/shop/screens/categories/edit_category/responsive_screens/edit_categories_tablet_screen.dart';
-import 'package:flutter/material.dart';
-import 'package:get/get.dart';
+import 'package:ecommerce_admin_panel/features/shop/models/category_model.dart';
 
 class EditCategoryScreen extends StatelessWidget {
   const EditCategoryScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    final category = Get.arguments;
+    final CategoryModel category = Get.arguments;
+
+    if (category == null) {
+      log("Error: No category data received in EditCategoryScreen!");
+      return Scaffold(
+        body: Center(child: Text("Error: No category data found")),
+      );
+    }
+
+    log("Category received: ${category.toString()}");
+
     return TSiteTemplate(
-      desktop: EditCategoriesDesktopScreen(
-        category: category,
-      ),
-      tablet: EditCategoriesTabletScreen(
-        category: category,
-      ),
-      mobile: EditCategoriesMobileScreen(
-        category: category,
-      ),
+      desktop: EditCategoriesDesktopScreen(category: category),
+      tablet: EditCategoriesTabletScreen(category: category),
+      mobile: EditCategoriesMobileScreen(category: category),
     );
   }
 }
