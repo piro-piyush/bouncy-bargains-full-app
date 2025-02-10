6d938e980df72653491fa905328db02cab644577
piyush <piyush72717272@gmail.com>
2025-02-10 22:12:37 +0530

    category shop brand

--- ecommerce_admin_panel/lib/features/shop/screens/brands/all_brands/responsive_screens/brands_mobile_screen.dart
+++ ecommerce_admin_panel/lib/features/shop/screens/brands/all_brands/responsive_screens/brands_mobile_screen.dart
@@ -1,17 +1,54 @@
+import 'package:ecommerce_admin_panel/common/widgets/breadcrumbs/breadcrumb_with_heading.dart';
+import 'package:ecommerce_admin_panel/common/widgets/containers/rounded_container.dart';
+import 'package:ecommerce_admin_panel/common/widgets/data_table/table_header.dart';
+import 'package:ecommerce_admin_panel/common/widgets/loaders/loader_animation.dart';
+import 'package:ecommerce_admin_panel/features/shop/controllers/brand/brand_controller.dart';
+import 'package:ecommerce_admin_panel/features/shop/screens/brands/all_brands/widgets/data_table.dart';
+import 'package:ecommerce_admin_panel/routes/routes.dart';
 import 'package:ecommerce_admin_panel/utils/constants/sizes.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 class BrandsMobileScreen extends StatelessWidget {
   const BrandsMobileScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final controller = Get.put(BrandController());
     return Scaffold(
       body: SingleChildScrollView(
         child: Padding(
           padding: const EdgeInsets.all(TSizes.defaultSpace),
           child: Column(
-            children: [],
+            children: [
+              // Breadcrumbs
+              TBreadcrumbWithHeading(
+                  heading: "Brand", breadcrumbItems: ["Brands"]),
+              SizedBox(
+                height: TSizes.spaceBtwSections,
+              ),
+
+              // Table Body
+              TRoundedContainer(
+                child: Column(
+                  children: [
+                    // Table Header
+                    TTableHeader(
+                      buttonText: "Create New Brand",
+                      onPressed: () => Get.toNamed(TRoutes.createBrand),
+                    ),
+                    SizedBox(
+                      height: TSizes.spaceBtwItems,
+                    ),
+
+                    // Table
+                    Obx(() => controller.isLoading.value
+                        ? TLoaderAnimation()
+                        : BrandTable()),
+                  ],
+                ),
+              )
+            ],
           ),
         ),
       ),
