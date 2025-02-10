91507eb08a96f5827a94251e636821d535100777
piyush <piyush72717272@gmail.com>
2025-02-10 22:12:31 +0530

    category model

--- ecommerce_admin_panel/lib/features/shop/models/brand_model.dart
+++ ecommerce_admin_panel/lib/features/shop/models/brand_model.dart
@@ -1,11 +1,18 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:ecommerce_admin_panel/features/shop/models/category_model.dart';
+import 'package:ecommerce_admin_panel/utils/formatters/formatter.dart';
 
 class BrandModel {
   String id;
   String name;
   String image;
   bool isFeatured;
-  int productsCount;
+  int? productsCount;
+  DateTime? createdAt;
+  DateTime? updatedAt;
+
+  // Not mapped
+  List<CategoryModel>? brandCategories;
 
   // Constructor
   BrandModel({
@@ -13,11 +20,22 @@ class BrandModel {
     required this.name,
     required this.image,
     this.isFeatured = false,
-    this.productsCount = 0,
+    this.productsCount,
+    this.createdAt,
+    this.updatedAt,
+    this.brandCategories,
   });
 
+  /// Empty Helper Function
+  static BrandModel empty() => BrandModel(id: "", name: "", image: "");
+
+  String get formattedDate => TFormatter.formatDate(createdAt);
+
+  String get formattedUpdatedDate => TFormatter.formatDate(updatedAt);
+
   // Factory method to create a BrandModel from Firestore DocumentSnapshot
-  factory BrandModel.fromSnapshot(DocumentSnapshot<Map<String, dynamic>> document) {
+  factory BrandModel.fromSnapshot(
+      DocumentSnapshot<Map<String, dynamic>> document) {
     if (document.data() != null) {
       final data = document.data()!;
       return BrandModel(
@@ -25,7 +43,8 @@ class BrandModel {
         name: data['Name'] ?? '',
         image: data['Image'] ?? '',
         isFeatured: data['IsFeatured'] ?? false,
-        productsCount: data.containsKey('ProductsCount') ? data['ProductsCount'] ?? 0 : 0,
+        productsCount:
+            data.containsKey('ProductsCount') ? data['ProductsCount'] ?? 0 : 0,
       );
     } else {
       return BrandModel.empty();
@@ -33,34 +52,32 @@ class BrandModel {
   }
 
   // Method to convert BrandModel to JSON
-  Map<String, dynamic> toJson() {
+  toJson() {
     return {
+      'Id': id,
       'Name': name,
       'Image': image,
       'IsFeatured': isFeatured,
-      'ProductsCount': productsCount,
+      'ProductsCount': productsCount = 0,
+      'CreatedAt': createdAt,
+      'UpdatedAt': updatedAt = DateTime.now(),
     };
   }
 
-  // Empty Brand Helper Function
-  static BrandModel empty() {
-    return BrandModel(
-      id: '',
-      name: '',
-      image: '',
-      isFeatured: false,
-      productsCount: 0,
-    );
-  }
-
   // Method to convert BrandModel from JSON (if needed in future for other scenarios)
-  factory BrandModel.fromJson(Map<String, dynamic> json) {
+  factory BrandModel.fromJson(Map<String, dynamic> document) {
+    final data = document;
+    if (data.isEmpty) return BrandModel.empty();
     return BrandModel(
-      id: json['id'] ?? '',
-      name: json['Name'] ?? '',
-      image: json['Image'] ?? '',
-      isFeatured: json['IsFeatured'] ?? false,
-      productsCount: json['ProductsCount'] ?? 0,
+      id: data['id'] ?? '',
+      name: data['Name'] ?? '',
+      image: data['Image'] ?? '',
+      isFeatured: data['IsFeatured'] ?? false,
+      productsCount: int.parse((data['ProductsCount'] ?? 0).toString()),
+      createdAt:
+          data.containsKey('CreatedAt') ? data['CreatedAt']?.toDate() : null,
+      updatedAt:
+          data.containsKey('UpdatedAt') ? data['UpdatedAt']?.toDate() : null,
     );
   }
 }
